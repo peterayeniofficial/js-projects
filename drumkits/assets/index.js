fd74bd5fa30d75d5cd6809f13a7960eeb58bad41
Peter Ayeni <ayenicology@gmail.com>
2019-05-03 00:15:02 +0100

    Update: Add playing function

---
+++ drumkits/assets/index.js
@@ -0,0 +1,16 @@
+window.addEventListener('keydown', function (e) {
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    console.log(key);
+    if (!audio) return; // if key does not exist return
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+})
+
+let keys = document.querySelectorAll('.key')
+
+keys.forEach(key => key.addEventListener('transitionend', function (e) {
+    if (e.propertyName !== 'transform') return; // return if the style is not transform
+    key.classList.remove('playing');
+}))
