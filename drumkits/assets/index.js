fbab0f430a29ad38c1d5d04892f2e5f9840c685e
Peter Ayeni <ayenicology@gmail.com>
2019-05-03 00:56:05 +0100

    Feat: Add footer section

--- drumkits/assets/index.js
+++ drumkits/assets/index.js
@@ -1,9 +1,9 @@
-// Play function
+// Drum Kits
+// By Peter Ayeni
 
 function playSound(e) {
     let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    console.log(key);
     if (!audio) return; // if key does not exist return
     audio.currentTime = 0;
     audio.play();
